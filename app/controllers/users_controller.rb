d6aafebdd620ba940c848a21b5334bc296cda1d7
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-04-28 09:30:52 -0700

    Update View: users show page, users can READ their expenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def show
+    # p params
+    @user = User.find_by(id: params[:id])
+    @expenses = @user.expenses
+    p '*'*100
+    p @expenses[0].amount
+    p '*'*100
   end
 end
