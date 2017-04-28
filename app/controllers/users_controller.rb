f07ad33559fa99bc2605575a39e7fece27049734
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-04-28 11:01:57 -0700

    update log

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user_expenses = @user.expenses
     @all_expenses = Expense.all
-
   end
 
   def current_user_home
     redirect_to current_user
   end
-
 end
