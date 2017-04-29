ecb51490979b06c6da09aa698bd83dafae11385b
Sanderfer Chau <sanderfer.chau@gmail.com>
2017-04-29 09:56:18 -0700

    Refactor expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,41 +1,42 @@
 class ExpensesController < ApplicationController
-  def new
-  end
 
   def create
-    expense = Expense.new(user_id: params[:user_id], description: params[:expense][:description], amount: params[:expense][:amount], date: params[:expense][:date], time: params[:expense][:time])
-    if expense.save
-      redirect_to user_path(params[:user_id])
-    else
-      redirect_to user_path(params[:user_id])
-    end
+    expense = Expense.new(user_id: params[:user_id], 
+                          description: params[:expense][:description], 
+                          amount: params[:expense][:amount], 
+                          date: params[:expense][:date], 
+                          time: params[:expense][:time])
+    
+    redirect_to current_user if expense.save
   end
 
-  def edit
-    @user = User.find_by(id: params[:user_id])
-  end
 
   def update
-    @expense = Expense.find_by(id: params[:id])
-    @user = User.find_by(id: params[:user_id])
+    expense = Expense.find_by(id: params[:id])
+    attribute_name = params[:expense].keys.first
+    attribute_val = params[:expense]
 
-    props = ['description','date', 'amount', 'time']
-    key = params[:expense].keys.first
+    change_attribute_value(expense, attribute_name, attribute_val)
 
-    props.each do |ans|
-      if ans == key
-        @expense[ans] = params[:expense][ans]
-        @expense.save
-      end
-    end
-    redirect_to user_path(@user)
+    redirect_to current_user
   end
 
   def destroy
-    @expense = Expense.find_by(id: params[:id])
-    @user = User.find_by(id: params[:user_id])
-    if @expense.destroy
-      redirect_to user_path(@user)
+    expense = Expense.find_by(id: params[:id])
+
+    redirect_to current_user if expense.destroy 
+  end
+
+  private
+
+  def change_attribute_value(exp, key, val) 
+    props = ['description','date', 'amount', 'time']
+
+    props.each do |ans|
+      if ans == key
+        exp[ans] = val[ans]
+        exp.save
+      end
     end
   end
 
